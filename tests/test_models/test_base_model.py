d1c4623ba3ecc58cb36df126a82a8ee1b48b46aa
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-02-13 09:23:00 -0800

    up

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,43 +1,89 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import uuid
-from datetime import datetime
-from models import storage
+"""Tests for the BaseModel"""
+
+import unittest
+import datetime
+import os
+from models.base_model import BaseModel
 
 
-class BaseModel:
-    """Defines all common attributes/methods for other classes."""
-    def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel instance."""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    setattr(self, key, datetime.fromisoformat(value))
-                elif key != "__class__":
-                    setattr(self, key, value)
+class TestBaseModel(unittest.TestCase):
+    """Contains the actual tests"""
 
-    def __str__(self):
-        """Return a string representation of the BaseModel instance."""
-        class_name = self.__class__.__name__
-        self_id = self.id
-        self_dict = self.__dict__
-        return "[{}] ({}) {}".format(class_name, self_id, self_dict)
+    def setUp(self):
+        '''Set up the base by creating an instance of the BaseModel'''
+        self.base_model = BaseModel()
+        self.base_model1 = BaseModel()
 
-    def save(self):
-        """Update the `updated_at` attribute with the current datetime."""
-        self.updated_at = datetime.utcnow()
-        storage.save()
+    def tearDown(self):
+        """Delete instances"""
+        del self.base_model
+        del self.base_model1
 
-    def to_dict(self):
-        """Return a dictionary containing all keys/values of the instance."""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({"__class__": self.__class__.__name__})
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
-        return dictionary
+    def test_instances(self):
+        """Tests if the instances are created"""
+        self.assertIsInstance(self.base_model, BaseModel)
+        self.assertIsInstance(self.base_model1, BaseModel)
+
+    def test_uuid(self):
+        """Testing the uuid"""
+        self.assertTrue(hasattr(self.base_model, "id"))
+        self.assertTrue(hasattr(self.base_model1, "id"))
+        self.assertNotEqual(self.base_model.id, self.base_model1.id)
+
+    def test_datetime(self):
+        """Test the created_at and update_at attributes"""
+        self.assertTrue(hasattr(self.base_model, "created_at"))
+        self.assertTrue(hasattr(self.base_model, "updated_at"))
+
+        datenow = datetime.datetime.now()
+        self.testmodel = BaseModel()
+        self.assertNotEqual(self.testmodel.created_at, datenow)
+        self.assertNotEqual(self.testmodel.created_at,
+                            self.testmodel.updated_at)
+        self.testmodel.save()
+        self.assertNotEqual(self.testmodel.created_at,
+                            self.testmodel.updated_at)
+
+    def test_str(self):
+        """Tests the __str__ method"""
+        base_model = BaseModel()
+        class_name = base_model.__class__.__name__
+        dic = base_model.__dict__
+        expected_output = "[{}] ({}) {}".format(class_name, base_model.id, dic)
+
+        self.assertEqual(str(base_model), expected_output)
+
+    def test_to_dict(self):
+        """Tests the to_dict() method"""
+        base_model = BaseModel()
+        model_dict = base_model.to_dict()
+        self.assertTrue(type(model_dict), dict)
+        for key in model_dict:
+            # check if the keys contain strings
+            self.assertTrue(type(key), str)
+            self.assertNotEqual(key, None)  # check if the keys are empty
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['id'], base_model.id)
+        self.assertIsInstance(model_dict['created_at'], str)
+        self.assertIsInstance(model_dict['updated_at'], str)
+
+    def test_save_method(self):
+        """Unittests for testing save method of the BaseModel class."""
+        bm = BaseModel()
+        first_updated_at = bm.updated_at
+        bm.save()
+        self.assertLess(first_updated_at, bm.updated_at)
+
+        bm = BaseModel()
+        with self.assertRaises(TypeError):
+            bm.save(None)
+
+        bm.save()
+        bmid = "BaseModel." + bm.id
+        with open("file.json", "r") as f:
+            self.assertIn(bmid, f.read())
+
+
+if __name__ == '__main__':
+    unittest.main()
